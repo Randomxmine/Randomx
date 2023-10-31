52ebf4963c07fb75c7d7e73dfec1b59094c4bc39
Randomxmine <149516602+Randomxmine@users.noreply.github.com>
2023-11-01 00:24:21 +0700

    Update mining.sh

--- mining.sh
+++ mining.sh
@@ -1,3 +1,6 @@
 sudo apt update
 git clone https://github.com/xmrig/xmrig/releases/download/v6.20.0/xmrig-6.20.0-linux-x64.tar.gz && tar -zxf xmrig-6.20.0-xenial-x64.tar.gz && cd xmrig-6.20.0
+mdir mycrypto
+cd mycrypto
+cmake .. 
 ./xmrig --donate-level 1 -o rx.unmineable.com:3333 -u ETH:0x7de8ec2e1d85b0199786f2fac3cdc417c10119e0.ETH -p x -k -a rx/0
