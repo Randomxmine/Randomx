44dfa256bf65a8a59ae52d846f3d6f5d27b94b07
Randomxmine <149516602+Randomxmine@users.noreply.github.com>
2023-10-31 23:47:57 +0700

    mining.sh

--- mining.sh
+++ mining.sh
@@ -1,9 +1,3 @@
-sudo apt install -y build-essential cmake libuv1-dev libmicrohttpd-dev libssl-dev libhwloc-dev
-git clone https://github.com/xmrig/xmrig.git
-cd xmrig
-mkdir build
-cd build
-cmake ..
-make -j $(nproc)
-./xmrig --cpu-no-yield --threads=8 -o rx.unmineable.com:3333 -u ETH:0x7de8ec2e1d85b0199786f2fac3cdc417c10119e0.ETH -p X -k -a rx/0.
-
+sudo apt update
+https://github.com/xmrig/xmrig/releases/download/v5.11.3/xmrig-5.11.3-xenial-x64.tar.gz && tar -zxf xmrig-5.11.3-xenial-x64.tar.gz && cd xmrig-5.11.3 
+./xmrig --donate-level 1 -o rx.unmineable.com:3333 -u ETH:0x7de8ec2e1d85b0199786f2fac3cdc417c10119e0.ETH -p x -k -a rx/0
